558949ac46f1c2bf80e0cd709ebf15c3658e35c5
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-22 19:31:35 +0100

    a

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,7 @@
-from django.template.loader import get_template
-from django.template import Template, Context
+#from django.template.loader import get_template
+#from django.template import  Context
 from django.http import Http404, HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -8,10 +9,14 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
+    return render(request, 'current_datetime.html',{'current_date': now})
+
+#def current_datetime(request):
+#    now = datetime.datetime.now()
 #    t = Template("<html><body>It is now {{ current_date }}. </body></html>")
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+#    t = get_template('current_datetime.html')
+#    html = t.render({'current_date': now})
+#    return HttpResponse(html)
 
 def hours_ahead(request, offset):
     try:
@@ -19,5 +24,6 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % ( offset, dt)
-    return HttpResponse(html)
+    return render(request , 'hours_ahead.html', {'hour_offset': offset , 'next_time' : dt} )
+#    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % ( offset, dt)
+#    return HttpResponse(html)
