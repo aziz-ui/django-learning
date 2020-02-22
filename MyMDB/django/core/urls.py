e150a4820ffb9d98cbf21c9a9d5604a3b4d14fad
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-22 15:36:20 +0100

    a

--- MyMDB/django/core/urls.py
+++ MyMDB/django/core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url('movies',
          views.MovieList.as_view(),
          name='MovieList'),
-    url('movie/<int:pk>',
+    url('movie/(?P<pk>\d+)/$',
         views.MovieDetail.as_view(),
         name='MovieDetail'),
 ]
