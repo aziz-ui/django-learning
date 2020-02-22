6f62263829401239af7f89d19238e7148adaa473
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-22 17:25:05 +0100

    a

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from mysite.views import hello , current_datetime
+from mysite.views import hello , current_datetime , hours_ahead
 
 urlpatterns = [
      url(r'^admin/', include(admin.site.urls)),
      url(r'^hello/$', hello),
-     url(r'^date/$' , current_datetime),
+     url(r'^time/$', current_datetime),
+     url(r'^time/plus/\d{1,2}/$' , hours_ahead),
 ]
