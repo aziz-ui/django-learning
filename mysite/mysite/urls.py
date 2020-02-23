0aa85690570042cdb036d65b0d9ec864701c18cc
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-23 18:52:34 +0100

    v

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from mysite.views import hello , current_datetime , hours_ahead
+from mysite.views import hello , current_datetime , hours_ahead , contact
 from books import views
 urlpatterns = [
      url(r'^admin/', include(admin.site.urls)),
@@ -8,5 +8,6 @@ urlpatterns = [
      url(r'^time/$', current_datetime),
      url(r'^time/plus/(\d{1,2})/$' , hours_ahead),
      url(r'^search-form/$', views.search_form),
-     url(r'^search/$', views.search), 
+     url(r'^search/$', views.search),
+     url(r'^contact/$', contact),
 ]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,8 +1,10 @@
-#from django.template.loader import get_template
-#from django.template import  Context
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 import datetime
+from mysite.forms import ContactForm
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+
 
 def hello(request):
     return HttpResponse("Hello world")
@@ -11,12 +13,6 @@ def current_datetime(request):
     now = datetime.datetime.now()
     return render(request, 'current_datetime.html',{'current_date': now})
 
-#def current_datetime(request):
-#    now = datetime.datetime.now()
-#    t = Template("<html><body>It is now {{ current_date }}. </body></html>")
-#    t = get_template('current_datetime.html')
-#    html = t.render({'current_date': now})
-#    return HttpResponse(html)
 
 def hours_ahead(request, offset):
     try:
@@ -25,5 +21,19 @@ def hours_ahead(request, offset):
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
     return render(request , 'hours_ahead.html', {'hour_offset': offset , 'next_time' : dt} )
-#    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % ( offset, dt)
-#    return HttpResponse(html)
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+    return render(request, 'contact_form.html', {'form': form})
