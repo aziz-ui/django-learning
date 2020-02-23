4a443856b58587256a01c67b8ce6880d117f7ee2
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-23 16:58:00 +0100

    a

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse 
 # Create your views here.
+
+def search_form(request):
+    return render(request, 'search_form.html')
+
+def search(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty form.'
+    return HttpResponse(message)
