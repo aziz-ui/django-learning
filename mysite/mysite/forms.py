531abdc7e0febd7073b5f2f533a8fbde6ff8cdaa
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-23 19:33:50 +0100

    a

--- mysite/mysite/forms.py
+++ mysite/mysite/forms.py
@@ -5,4 +5,4 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     emal = forms.EmailField(required=False)
     message = forms.CharField(widget=forms.Textarea)
-    ip_addresse = forms.GenericIPAddressField(unpack_ipv4=True)
+    ip_addresse = forms.GenericIPAddressField()
