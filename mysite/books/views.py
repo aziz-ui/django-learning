b4893a27b828038ae51fd612baef571c3e4a3c8c
BENYAHIA Aziz <aziz.benyahia@hermes.com>
2020-02-23 17:25:47 +0100

    a

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -2,15 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from books.models import Book
+
 # Create your views here.
 
 def search_form(request):
     return render(request, 'search_form.html')
 
 def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__contains=q)
+        return render (request , 'search_reults.html', {'books': books, 'query': q})
     else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        return HttpResponse('Please submit a search term.')
